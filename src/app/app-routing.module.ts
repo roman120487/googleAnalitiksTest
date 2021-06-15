1a7d7160fe50831b32033a0c010dbaa43470a5e9
roman120487 <roman120487@ukr.net>
2021-06-15 14:09:26 +0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
